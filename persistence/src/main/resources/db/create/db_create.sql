03234424159702fc4bd65d75b23ffa837d38f754
Piotr Kamiński <piotr.kaminski@billongroup.com>
2023-06-19 23:26:31 +0200

    clean configuration

--- persistence/src/main/resources/db/create/db_create.sql
+++ persistence/src/main/resources/db/create/db_create.sql
@@ -2,5 +2,3 @@ CREATE USER "pi_kursy" LOGIN ENCRYPTED PASSWORD 'pi_kursy' VALID UNTIL 'infinity
 CREATE DATABASE "pi_kursy" WITH OWNER = "pi_kursy" ENCODING = 'UTF8' CONNECTION LIMIT = 100;
 
 GRANT ALL ON DATABASE "pi_kursy" TO "pi_kursy";
-
-ALTER ROLE "pi_kursy" SET search_path TO "pi_kursy";
