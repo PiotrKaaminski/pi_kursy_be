f137faf88f05f6d900b9e45f0064b673b027c46f
piotrk <piotr.kaminski@billongroup.com>
2023-08-18 21:17:45 +0200

    change initial user's password to "admin"

--- persistence/src/main/resources/db/initialData/insertUser.sql
+++ persistence/src/main/resources/db/initialData/insertUser.sql
@@ -1,4 +1,4 @@
 insert into users(id, username, password, role, creation_date)
 values
--- user with login "admin" and password "temp", requires changing password on first login
-('1bb60d65-056f-49bd-a483-a2f0be1a4b2b', 'admin', '$2a$12$WTSArclcdErD/lwMXbUW..WpK5OizSbghDW481dw5oH8yfb8KP4DG', 'ADMIN', current_timestamp(0));
+-- user with login "admin" and password "admin"
+('1bb60d65-056f-49bd-a483-a2f0be1a4b2b', 'admin', '$2a$12$5taySIA4TLLgUD5zQwjCIO7DHM4.bbQDb4wlMKhIW632Hb4zdghgC', 'ADMIN', current_timestamp(0));
